6c0a52b3f93b92b7b0b79b1d06b019ad8830fffd
Beenz <CH.SABEEN@GMAIL.COM>
2023-05-28 16:49:51 -0700

    navbar added

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,1 +1,15 @@
-export default function About() {}
+import React from "react";
+
+export default function About() {
+  return (
+    <div class="profile">
+      <img class="profile-pic" src="images/profile-pic.JPG" alt="profile" />
+
+      <div class="greeting">
+        <h1>Hello, I'm Sabeen</h1>
+
+        <p>Aspiring Web Developer</p>
+      </div>
+    </div>
+  );
+}
